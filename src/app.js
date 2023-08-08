94c66d3cb61c7b1391402d66b2a661e8829e90d2
sathwikeddykomandla <sathwikreddykomandla@gmail.com>
2023-08-08 13:30:59 +0530

    ïnitial commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express=require('express')
+const  Student=require('./models/students')
+require('./db/connection')
+const app=express()
+
+// middleware
+app.use(express.json())
+
+// Create new students
+app.post('/students',(req,res)=>{
+    const student=new Student(req.body);
+    student.save().then(()=>{
+        console.log("obj created")
+        res.status(201).send(student)
+    }).catch((err)=>{
+        console.log(err)
+        res.status(400).send(err)
+    })
+})
+
+
+//get all students
+// Create new students
+app.get('/students',(req,res)=>{
+    student.save().then(()=>{
+        console.log("obj created")
+        res.status(201).send(student)
+    }).catch((err)=>{
+        console.log(err)
+        res.status(500).send(err)
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Listening at port 3000")
+})
