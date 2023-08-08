13cd33bdd67ea59ae5c0efca5e1fd0c3fe76fb0b
sathwikeddykomandla <sathwikreddykomandla@gmail.com>
2023-08-08 14:12:36 +0530

    API get requests

--- src/app.js
+++ src/app.js
@@ -7,16 +7,61 @@ const app=express()
 app.use(express.json())
 
 // Create new students
-app.post('/students',(req,res)=>{
-    const student=new Student(req.body);
-    student.save().then(()=>{
-        console.log("obj created")
-        res.status(201).send(student)
-    }).catch((err)=>{
-        console.log(err)
-        res.status(400).send(err)
-    })
+// app.post('/students',(req,res)=>{
+//     const student=new Student(req.body);
+//     student.save().then(()=>{
+//         console.log("obj created")
+//         res.status(201).send(student)
+//     }).catch((err)=>{
+//         console.log(err)
+//         res.status(400).send(err)
+//     })
+// })
+
+
+// Create new Students using Async Await
+app.post('/students',async(req,res)=>{
+    
+    try{
+    const student=new Student(req.body)
+    const createStudent=await student.save()
+    res.status(201).send(createStudent)
+    }
+    catch(e){
+        res.status(400).send(e)
+    }    
 })
+
+
+// get all students
+app.get('/students',async(req,res)=>{
+    try{
+        const students=await Student.find()
+        res.status(200).send(students)
+    }
+    catch(e){
+        
+        console.log(e)
+        res.status(400).send(e)
+    } 
+})
+
+// get individual student
+app.get('/students/:id',async(req,res)=>{
+    try{
+        // const students=await Student.findOne({_id:req.params.id})
+        const student=await Student.findById({_id:req.params.id})
+        if(!student){
+            return res.status(404).send();
+        }else{
+        res.status(200).send(student)
+        }    
+    }
+    catch(e){
+        res.status(400).send(e)
+    } 
+})
+
 
 
 app.listen(3000,()=>{
